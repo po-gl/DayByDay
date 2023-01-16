e0ac227a618db4b3947b7ab214d6a1be771e3a6b
po-gl <porterglines@gmail.com>
2023-01-16 14:24:02 -0700

    Remove unused code

--- DayByDay/Helpers/Haptics.swift
+++ DayByDay/Helpers/Haptics.swift
@@ -5,63 +5,8 @@
 //  Created by Porter Glines on 1/9/23.
 //
 
-import Foundation
-import CoreHaptics
 import UIKit
 
-
-class Haptics {
-    private var engine: CHHapticEngine?
-    
-    public func prepareHaptics() {
-        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
-        
-        do {
-            engine = try CHHapticEngine()
-            try engine?.start()
-        } catch {
-            print("There was an error creating the haptics engine: \(error.localizedDescription)")
-        }
-        engine?.isAutoShutdownEnabled = true
-        engine?.stop()
-    }
-    
-    
-    public func workHaptic() {
-        multiHaptic(4, 0.1, 0.07, 0.5, 1.0)
-    }
-    
-    
-    private func multiHaptic(_ count: Int,
-                     _ duration: Double, _ seperationDuration: Double,
-                     _ intensity: Float, _ sharpness: Float) {
-        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
-        
-        var events: [CHHapticEvent] = []
-        
-        let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: intensity)
-        let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: sharpness)
-        for i in 0..<count {
-            events.append(CHHapticEvent(eventType: .hapticContinuous,
-                                        parameters: [intensity, sharpness],
-                                        relativeTime: Double(i) * (duration + seperationDuration),
-                                        duration: duration))
-        }
-        
-        do {
-            let pattern = try CHHapticPattern(events: events, parameters: [])
-            let player = try engine?.makePlayer(with: pattern)
-            engine?.notifyWhenPlayersFinished { error in
-                self.engine?.stop()
-                return .stopEngine
-            }
-            try engine?.start()
-            try player?.start(atTime: CHHapticTimeImmediate)
-        } catch {
-            print("Failed to play pattern: \(error.localizedDescription)")
-        }
-    }
-}
 
 public func basicHaptic() {
     let generator = UIImpactFeedbackGenerator(style: .medium)
