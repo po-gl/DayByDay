f1c898ccfe6eee8b365115ba08e366dc952f1f6f
po-gl <porterglines@gmail.com>
2023-02-09 20:48:18 -0700

    Change eye animation and add shadow

--- DayByDay/Helpers/WigglyEyes.swift
+++ DayByDay/Helpers/WigglyEyes.swift
@@ -14,6 +14,7 @@ struct WigglyEyes: View {
     var eyeWidth = 38.0
     var eyeDepth = 8.0
     
+    let animateOffset = 2.0
     let offset = 4.0
     
     @State var animate = false
@@ -23,12 +24,12 @@ struct WigglyEyes: View {
             HStack(spacing: 25) {
                 pairOfEyes(width: eyeWidth +  0, depth: eyeDepth +  0)
                     .offset(x: offset)
-                    .offset(x: animate ? -1 : 1)
+                    .offset(x: animate ? -animateOffset : animateOffset)
                 pairOfEyes(width: eyeWidth + 20, depth: eyeDepth + 10)
-                    .offset(x: animate ? -1 : 1)
+                    .offset(x: animate ? -animateOffset : animateOffset)
                 pairOfEyes(width: eyeWidth + 20, depth: eyeDepth + 10)
                     .offset(x: -offset)
-                    .offset(x: animate ? 1 : -1)
+                    .offset(x: animate ? animateOffset : -animateOffset)
             }
             Spacer()
         }
@@ -41,9 +42,11 @@ struct WigglyEyes: View {
         HStack {
             Circle()
                 .frame(width: eyeSize)
+                .shadow(radius: 5)
                 .padding(.trailing, width)
             Circle()
                 .frame(width: eyeSize)
+                .shadow(radius: 5)
         }
         .frame(width: barWidth)
         .padding(.top, depth)
