a38a8cd017189746087d525b43d5f8b5d8eef372
po-gl <porterglines@gmail.com>
2023-02-23 22:58:15 -0700

    Add simple category gradients

--- DayByDay/Extensions/LinearGradient+ForCategory.swift
+++ DayByDay/Extensions/LinearGradient+ForCategory.swift
@@ -16,12 +16,6 @@ extension LinearGradient {
                               .init(color: Color(hex: 0xB04386), location: 0.8),
                               .init(color: Color(hex: 0xB3F2B7), location: 1.0)],
                       startPoint: .leading, endPoint: .trailing)
-        case .productive:
-            self.init(stops: [.init(color: Color(hex: 0xC62379), location: 0.0),
-                              .init(color: Color(hex: 0xF77756), location: 0.2),
-                              .init(color: Color(hex: 0xA8E712), location: 0.6),
-                              .init(color: Color(hex: 0xD8F7EC), location: 1.1)],
-                      startPoint: .leading, endPoint: .trailing)
         case .creative:
             self.init(stops: [.init(color: Color(hex: 0xFCEBD6), location: 0.0),
                               .init(color: Color(hex: 0xBAE1E5), location: 0.2),
@@ -29,6 +23,32 @@ extension LinearGradient {
                               .init(color: Color(hex: 0xC33FDB), location: 0.8),
                               .init(color: Color(hex: 0xF0BE83), location: 1.1)],
                       startPoint: .leading, endPoint: .trailing)
+        case .productive:
+            self.init(stops: [.init(color: Color(hex: 0xC62379), location: 0.0),
+                              .init(color: Color(hex: 0xF77756), location: 0.2),
+                              .init(color: Color(hex: 0xA8E712), location: 0.6),
+                              .init(color: Color(hex: 0xD8F7EC), location: 1.1)],
+                      startPoint: .leading, endPoint: .trailing)
+        }
+    }
+    
+    init(forSimple category: StatusCategory) {
+        switch category {
+        case .active:
+            self.init(stops: [.init(color: Color(hex: 0xE69F1E), location: 0.0),
+                              .init(color: Color(hex: 0xE63C5C), location: 0.5),
+                              .init(color: Color(hex: 0xB04386), location: 1.0)],
+                      startPoint: .topLeading, endPoint: .bottomTrailing)
+        case .creative:
+            self.init(stops: [.init(color: Color(hex: 0xBAE1E5), location: -0.2),
+                              .init(color: Color(hex: 0xBE59D5), location: 0.5),
+                              .init(color: Color(hex: 0xF0BE83), location: 1.2)],
+                      startPoint: .bottomLeading, endPoint: .topTrailing)
+        case .productive:
+            self.init(stops: [.init(color: Color(hex: 0xFF7676), location: 0.0),
+                              .init(color: Color(hex: 0x97D327), location: 0.5),
+                              .init(color: Color(hex: 0xFFF074), location: 1.1)],
+                      startPoint: .bottom, endPoint: .top)
         }
     }
 }
