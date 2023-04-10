f287f80ac657fe3fa7251119260677b6cb97bcb8
po-gl <porterglines@gmail.com>
2023-04-10 12:31:21 -0600

    Add shine animation to underline

--- DayByDay/DateView.swift
+++ DayByDay/DateView.swift
@@ -15,6 +15,8 @@ struct DateView: View {
     
     var formatter = dayFormatter
     
+    @State var animate = false
+    
     var body: some View {
         VStack (spacing: 8) {
             Text("\(date, formatter: formatter)")
@@ -22,30 +24,56 @@ struct DateView: View {
             Underline()
         }
         .frame(height: width)
+        
+        .onAppear {
+            animate = true
+        }
     }
     
     @ViewBuilder
     private func Underline() -> some View {
         ZStack {
-            HorizontalLine()
-                .stroke(.primary, style: StrokeStyle(lineWidth: 3, lineCap: .round))
-                .frame(width: width, height: 1)
-            ShineLines()
-                .offset(x: shineOffset)
+            BaseLine()
+            
+            ShineLines(width: 20)
+                .offset(x: animate ? shineOffset : -200)
+                .animation(.easeInOut(duration: 0.9).delay(0.35), value: animate)
+                .mask(BaseLine())
+            
+            AnimatedShine()
+                .mask(BaseLine())
         }
     }
     
     @ViewBuilder
-    private func ShineLines() -> some View {
+    private func BaseLine() -> some View {
+        HorizontalLine()
+            .stroke(.primary, style: StrokeStyle(lineWidth: 3, lineCap: .round))
+            .frame(width: width, height: 1)
+    }
+    
+    @ViewBuilder
+    private func ShineLines(width: Double = 20) -> some View {
         ZStack {
             HorizontalLine()
                 .stroke(.background, style: StrokeStyle(lineWidth: 2, lineCap: .butt))
-                .frame(width: 20, height: 1)
+                .frame(width: width, height: 1)
             HorizontalLine()
                 .stroke(.background, style: StrokeStyle(lineWidth: 2, lineCap: .butt))
-                .frame(width: 5, height: 1)
-                .offset(x: 18)
+                .frame(width: width/4, height: 1)
+                .offset(x: width-2)
         }
+    }
+    
+    @ViewBuilder
+    private func AnimatedShine() -> some View {
+        ShineLines(width: 80)
+            .offset(x: animate ? 200 : -200)
+            .animation(.easeInOut(duration: 0.9).delay(0.0), value: animate)
+        
+        ShineLines(width: 30)
+            .offset(x: animate ? 200 : -200)
+            .animation(.easeInOut(duration: 1.0).delay(0.3), value: animate)
     }
 }
 
